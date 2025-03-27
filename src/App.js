18cd9c546a28a442b8adc8e5a497860510e22344
Rodrigo Yael M. V <yava1938@outlook.com>
2025-03-27 16:44:53 -0600

    carga inicial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Chatbot} from "./views/Chatbot.jsx";
+
+function App() {
+    return (
+        <div>
+            <Chatbot />
+        </div>
+    );
+}
+
+export default App;
